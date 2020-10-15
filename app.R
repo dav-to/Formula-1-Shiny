a4118ea8efb476af6591624c7e70cfdcc99a894f
dav-to <42210889+dav-to@users.noreply.github.com>
2020-10-15 18:11:41 +0200

    Commit 13

--- app.R
+++ app.R
@@ -10,12 +10,12 @@ source('plotFunction.R')
 
 
 ui <- fluidPage(fluidRow(column(1, offset = 4, tags$br(), tags$img(height=50, width = 100, src = "checker-1648337_640.png")),
-                         column(5,tags$br(),tags$h1("Formula One Standings", style="color:#cc0000;", style="font-family:Impact"))
+                         column(5,tags$br(),tags$h1("Formula 1 Standings", style="color:#cc0000;", style="font-family:Impact"))
                 ), fluidRow(tags$br(),tags$br(),tags$br()), fluidRow(column(4, tags$br(), wellPanel(sliderInput(inputId = "year",
                             label = "Select a season", 
                             value = 2020, min = 1950, max = 2020, step = 1, sep=""),
                 sliderInput(inputId = "top",
-                            label = "Select number of drivers\\teams to show", 
+                            label = "Select number of drivers\\constructors to show", 
                             value = 5, min = 1, max = 10, step = 1, sep=""),
                 radioButtons("charttype", label = "Driver or Constructor",
                              choices = list("Driver" = "A", "Constructor" = "B"), selected = "A"),
